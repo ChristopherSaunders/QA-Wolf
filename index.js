4a18fc1ed62b029abb96ddb2996abc51994e2ab4
ChristopherSaunders <Christopher_Saunders92@yahoo.com>
2025-11-11 15:38:46 -0500

    QA Wolf Take Home Quiz

---
+++ index.js
@@ -0,0 +1,105 @@
+// EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
+const { chromium } = require("playwright");
+const { expect } = require("playwright/test");
+
+async function sortHackerNewsArticles() {
+  // launch browser
+  const browser = await chromium.launch({ headless: false });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const pagelink = "https://news.ycombinator.com/newest";
+
+  // go to Hacker News
+  let articles = [];
+  await page.goto(pagelink);
+  await checkArticles(page, articles);
+  await finishTest(browser,articles); 
+}
+
+(async () => {
+  await sortHackerNewsArticles();
+})();
+
+
+async function checkArticles(page, articles){
+  const bigbox = await page.locator("#bigbox");
+  const ranks = await bigbox.locator(".rank").allInnerTexts();
+  const titles = await bigbox.locator(".athing").locator(".titleline").allInnerTexts();
+  const ages = await bigbox.locator(".age").all();
+
+  if(!await compareArticles({ranks: ranks,titles: titles,ages: ages}, articles)) 
+    return;
+
+  if(articles.length < 100){
+    await bigbox.locator(".morelink").click();
+    await checkArticles(page,articles);
+  }
+}
+
+async function finishTest(browser, data=[]){
+  console.log(data);
+  await browser.close();
+}
+
+async function checkLength(articleInfo){
+  try{
+    await expect(articleInfo.titles.length === articleInfo.ages.length
+              && articleInfo.titles.length === articleInfo.ranks.length)
+       .toBeTruthy();
+    return true;
+  }
+  catch{
+    console.error({
+      Error: "Articles did not load as expected",
+      titles_length: articleInfo.titles.length,
+      rank_length: articleInfo.ranks.length,
+      ages_length: articleInfo.ages.length,
+      expected: "Elements to be the should be the same length"
+    });
+    return false;
+  }
+}
+
+async function compareArticles(articlesInfo, sortedArticles){
+   if(!await checkLength(articlesInfo))
+    return false;
+
+  let index = 0;
+  for(let item of articlesInfo.ages){
+    let time = await item.getAttribute('title');
+    let art_time = time.split(" ")[0];
+    let secs = parseInt(time.split(" ")[1]);
+
+    if(sortedArticles.length < 100){
+      if(sortedArticles.length > 0){
+        try{
+          await expect(sortedArticles[sortedArticles.length-1].age >= secs).toBeTruthy();
+        }
+        catch{
+          console.error({
+            Error: "Articles out of Order",
+              Current_Article: {
+             title: articlesInfo.titles[index], 
+             rank: articlesInfo.ranks[index], 
+             article_time: art_time, 
+             age: secs
+            },
+            Previous_Article: sortedArticles[sortedArticles.length-1],
+            expected: `"${sortedArticles[sortedArticles.length-1].rank} ${sortedArticles[sortedArticles.length-1].title}" needs to occur (AFTER/SAME TIME) as "${articlesInfo.ranks[index]} ${articlesInfo.titles[index]}"`
+          });
+          return false;
+        }
+      }
+       sortedArticles.push({
+          title: articlesInfo.titles[index], 
+          rank: articlesInfo.ranks[index], 
+          article_time: art_time, 
+          age: secs
+        });
+        index++;
+      }
+      else {break;}
+    }
+    return true;
+  }
+    
